76f319244e3ec4df9c071b345d9e0daa908a1f1b
jaeyow <jaeyow@gmail.com>
2023-06-30 16:40:39 +1000

    update frontend to point to the SAM container based lambda to get around implicit library error

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,27 +1,23 @@
 import axios from 'axios'
 
 import { stringify } from "qs"
-import aws_exports from '../aws-exports'
 import { IGetProductsResponse } from 'models'
 
 /** Pass category and/or user_id (to pass to Amazon Personalize) */
 export const getProducts = async (filters?: string[], user_id?: string) => {
     let response: IGetProductsResponse
+    const products_endpoint = `https://58spt4nge9.execute-api.ap-southeast-2.amazonaws.com/Prod/products`
 
-    if (filters || user_id) {
+    if (filters?.length || user_id) {
         const params = stringify({
                 user_id,
                 filters,
             },
             { skipNulls: true })
 
-        response = await axios.get(
-            `${aws_exports.aws_cloud_logic_custom[0].endpoint}/products?${params}`
-        )
+        response = await axios.get(`${products_endpoint}?${params}`)
     } else {
-        response = await axios.get(
-            `${aws_exports.aws_cloud_logic_custom[0].endpoint}/products`
-        )
+        response = await axios.get(products_endpoint)
     }
 
     const data = response.data || []
